b8aa3ded7519d321b480e6eb04feb46295fe9a50
alice <alice.nthenge@student.moringaschool.com>
2025-03-20 23:02:30 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(books => {
+      console.log("Fetched Books:", books);
+
+      
+      const fifthBook = books[4]; 
+      console.log("5th Book:", fifthBook.name);
+
+      
+      fetch("https://anapioficeandfire.com/api/characters/1031")
+        .then(response => response.json())
+        .then(character => console.log("1031st Character:", character.name || "No Name"));
+
+      
+      const totalPages = books.reduce((sum, book) => sum + book.numberOfPages, 0);
+      console.log("Total Pages in All Books:", totalPages);
+
+      renderBooks(books);
+    })
+    .catch(error => console.error("Error fetching books:", error));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector("main");
   books.forEach(book => {
-    const h2 = document.createElement('h2');
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
+
+    
+    
+    
